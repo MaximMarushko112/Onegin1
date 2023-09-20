4732b1f2cc0f1fbc4945e03c30f2e1fcac1b04d2
kirilliliych <kirill.iliych@gmail.com>
2023-09-21 01:06:31 +0300

    code review

--- Qsort.cpp
+++ Qsort.cpp
@@ -16,7 +16,7 @@ size_t my_qsort(char* arr[], size_t left, size_t right,
             swap(arr, left, right);
         return 2;
     }
-    size_t mid = 0;
+    size_t mid = 0;                                             // unite lines 19 and 20
     mid = partition(arr, left, right, comp);
     my_qsort(arr, left, mid, comp);
     my_qsort(arr, mid + 1, right, comp);
@@ -31,8 +31,8 @@ void swap(char* arr[], size_t left, size_t right) {
 
 size_t partition(char* arr[], size_t left, size_t right,
                  int (*comp)(const char*, const char*)) {
-    size_t mid = (left + right) / 2;
-    char* separator = arr[mid];
+    size_t mid = (left + right) / 2;                            // left + (right - left) / 2 to avoid overflow
+    char* separator = arr[mid];                                 // why not just random element?
     while (left <= right) {
         while ((*comp)(arr[left], separator) == -1)
             left++;
@@ -50,9 +50,9 @@ size_t partition(char* arr[], size_t left, size_t right,
 }
 
 int str_compare_lr(const char* str1, const char* str2) {
-    size_t ch1 = 0, ch2 = 0;
+    size_t ch1 = 0, ch2 = 0;            // if those are not chars, then index1 and index2?
     while (str1[ch1] != '\0' && str2[ch2] != '\0') {
-        while (!isalpha(str1[ch1]))
+        while (!isalpha(str1[ch1]))     // what if str1 like " "? str1[ch1] != '\0' when ch1 == 0, but then you'll go out of str bounds because isalpha('\0') == 0 
             ch1++;
         while (!isalpha(str2[ch2]))
             ch2++;
@@ -60,20 +60,20 @@ int str_compare_lr(const char* str1, const char* str2) {
             break;
 
         int alpha1 = 0, alpha2 = 0;
-        if (isupper(str1[ch1]))
-            alpha1 = str1[ch1] - 'A';
-        else
-            alpha1 = str1[ch1] - 'a';
-
-        if (isupper(str2[ch2]))
-            alpha2 = str2[ch2] - 'A';
-        else
-            alpha2 = str2[ch2] - 'a';
-
-        if (alpha1 > alpha2)
-            return 1;
-        else if (alpha1 < alpha2)
-            return -1;
+        if (isupper(str1[ch1]))         //
+            alpha1 = str1[ch1] - 'A';   //
+        else                            //
+            alpha1 = str1[ch1] - 'a';   //
+                                        //
+        if (isupper(str2[ch2]))         //
+            alpha2 = str2[ch2] - 'A';   //  instead of all this you could just compare toupper (or tolower) results
+        else                            //
+            alpha2 = str2[ch2] - 'a';   //
+                                        //
+        if (alpha1 > alpha2)            //
+            return 1;                   //
+        else if (alpha1 < alpha2)       //
+            return -1;                  //
         else {
             ch1++;
             ch2++;
@@ -88,7 +88,8 @@ int str_compare_lr(const char* str1, const char* str2) {
 }
 
 int str_compare_rl(const char* str1, const char* str2) {
-    int ch1 = strlen(str1), ch2 = strlen(str2);
+    int ch1 = strlen(str1), ch2 = strlen(str2);             // strlen is compelled to go through all string length to compute, it is really slow;
+                                                            // would be no need in this if you saved length of each string in struct 
     while (ch1 >= 0 && ch2 >= 0) {
         while (!isalpha(str1[ch1]) && ch1 >= 0)
             ch1--;
@@ -117,7 +118,7 @@ int str_compare_rl(const char* str1, const char* str2) {
             ch2--;
         }
     }
-    if (ch1 < 0 && ch2 < 0)
+    if (ch1 < 0 && ch2 < 0)  
         return 0;
     else if (ch1 < 0 && ch2 >= 0)
         return -1;
